aac1b5c898c139fef662a129683863be388e307f
NaidaDV <62427073+NaidaDV@users.noreply.github.com>
2020-10-04 21:53:14 +0300

    Update webhook.tf

--- terraform/webhook.tf
+++ terraform/webhook.tf
@@ -1,8 +1,6 @@
 resource "github_repository_webhook" "jenkins_hook" {
   repository = "biotestmine"
 
-#  name = "web"
-
   configuration {
     url          = "http://${aws_instance.ci.public_ip}:8080/github-webhook/"
     content_type = "form"
