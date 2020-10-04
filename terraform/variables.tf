5499cf1dcc52ed4bbbd433a64cd8885c75b6a847
NaidaDV <62427073+NaidaDV@users.noreply.github.com>
2020-10-04 22:00:17 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,8 @@ variable "ami" {}
 variable "access_key" {}
 variable "secret_key" {}
 variable "git_token" {}
+variable "ssh_user" {}
+variable "privet_key" {}
 variable "auth" {}
 variable "aws_region" {}
 variable "aws_AZ1" {}
