7015dbefe1c90ca4881195003958acff37d3a67e
NaidaDV <62427073+NaidaDV@users.noreply.github.com>
2020-10-06 16:25:08 +0300

    Update subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,13 +1,34 @@
 #############################################
-resource "aws_subnet" "FT_subnet" {
+resource "aws_subnet" "FT_PROD_subnet" {
   vpc_id     = data.aws_vpc.default.id
   cidr_block = "172.31.1.0/24"
   availability_zone = var.aws_AZ1
   map_public_ip_on_launch = true
   
   tags = {
-    Name = "FT_subnet"
+    Name = "FT_PROD_subnet"
   }
 }
 #############################################
-
+resource "aws_subnet" "FT_DEV_subnet" {
+  vpc_id     = data.aws_vpc.default.id
+  cidr_block = "172.31.1.0/24"
+  availability_zone = var.aws_AZ1
+  map_public_ip_on_launch = true
+  
+  tags = {
+    Name = "FT_DEV_subnet"
+  }
+}
+#############################################
+resource "aws_subnet" "FT_CI_subnet" {
+  vpc_id     = data.aws_vpc.default.id
+  cidr_block = "172.31.1.0/24"
+  availability_zone = var.aws_AZ1
+  map_public_ip_on_launch = true
+  
+  tags = {
+    Name = "FT_CI_subnet"
+  }
+}
+#############################################
