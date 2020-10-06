417c37a2a6eb867098b708ef9ef7d9ead4d8c38f
NaidaDV <62427073+NaidaDV@users.noreply.github.com>
2020-10-06 16:52:49 +0300

    Update security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,7 +1,7 @@
 ###############################################
-resource "aws_security_group" "FT_security_group" {
-  name        = "FT_security_group"
-  description = "Allow traffic on port 80, 3306 + ICMP and SSH for checks"
+resource "aws_security_group" "FT_security_group_CI" {
+  name        = "FT_security_group_CI"
+  description = "Security group for CI"
   vpc_id      = data.aws_vpc.default.id
   ingress {
     description = "Allow http traffic on port 80"
@@ -16,13 +16,6 @@ resource "aws_security_group" "FT_security_group" {
     to_port     = 22 
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]              
-  }
-  ingress {
-    description = "Allow ICMP traffic"
-    from_port   = "-1"
-    to_port     = "-1"
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "Allow traffic on port 8080"
@@ -41,3 +34,55 @@ resource "aws_security_group" "FT_security_group" {
     Name = "FT_security_group"
   }
 }
+###############################################
+resource "aws_security_group" "FT_security_group_APP" {
+  name        = "FT_security_group_APP"
+  description = "Security group for application"
+  vpc_id      = data.aws_vpc.default.id
+  ingress {
+    description = "Allow ssh traffic on port 22"
+    from_port   = 22
+    to_port     = 22 
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]              
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "FT_security_group"
+  }
+}
+###############################################
+resource "aws_security_group" "FT_security_group_LOAD" {
+  name        = "FT_security_group_LOAD"
+  description = "Security group for loadbalancer"
+  vpc_id      = data.aws_vpc.default.id
+  ingress {
+    description = "Allow http traffic on port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow ssh traffic on port 22"
+    from_port   = 22
+    to_port     = 22 
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]              
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "FT_security_group"
+  }
+}
+###############################################
